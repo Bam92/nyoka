297172b2e453dccb18a72b9d0cc3fefe9c286fdf
Abel <test@t.cd>
2021-02-24 08:22:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('name', help='name of the user')
+
+parser.add_argument('-g', '--greeting',
+default='Hello',
+help='optional alternate greeting')
+
+args = parser.parse_args()
+
+print("{greeting}, {name}!".format(
+    greeting=args.greeting,
+    name=args.name
+))
+
